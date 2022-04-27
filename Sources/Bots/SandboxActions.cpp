57adb341c4ec6d0484ed9d075a0eab35448261db
Dreamy Cecil <stalker27production@gmail.com>
2022-04-27 17:06:25 +0300

    Add lock entity to the navmesh point info command.

--- Sources/Bots/SandboxActions.cpp
+++ Sources/Bots/SandboxActions.cpp
@@ -384,6 +384,7 @@ static void CECIL_NavMeshPointInfo(void) {
   CPrintF("^cffffff%d\n", pbpp->bpp_iIndex); // point index on top
 
   CEntity *penImportant = pbpp->bpp_penImportant;
+  CEntity *penLock = pbpp->bpp_penLock;
   
   CPrintF("Connections: %d\n", pbpp->bpp_cbppPoints.Count());
   CPrintF("Pos:    %.2f, %.2f, %.2f\n", pbpp->bpp_vPos(1), pbpp->bpp_vPos(2), pbpp->bpp_vPos(3));
@@ -391,6 +392,14 @@ static void CECIL_NavMeshPointInfo(void) {
   CPrintF("Flags:  %s\n", ULongToBinary(pbpp->bpp_ulFlags));
   CPrintF("Entity: \"%s\"\n", (penImportant == NULL) ? "<none>" : penImportant->GetName());
   CPrintF("Next:   %d\n", (pbpp->bpp_pbppNext == NULL) ? -1 : pbpp->bpp_pbppNext->bpp_iIndex);
+  CPrintF("Lock:   \"%s\"\n", (penLock == NULL) ? "<none>" : penLock->GetName());
+
+  if (penLock != NULL) {
+    FLOAT3D &vPos = pbpp->bpp_plLockOrigin.pl_PositionVector;
+    ANGLE3D &aRot = pbpp->bpp_plLockOrigin.pl_OrientationAngle;
+    CPrintF("Origin: [%.2f, %.2f, %.2f], [%.2f, %.2f, %.2f]\n",
+            vPos(1), vPos(2), vPos(3), aRot(1), aRot(2), aRot(3));
+  }
 };
 
 // [Cecil] 2021-06-12: Connect current NavMesh point to another one
